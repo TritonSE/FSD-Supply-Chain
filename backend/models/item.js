274e4d042a4ad2f001bc44b451abd90272cf8a21
Ly <64985802+lynguyxn@users.noreply.github.com>
2021-01-20 14:45:22 -0800

    Updated schemas to const

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var BatchSchema = new Schema({
+const BatchSchema = new Schema({
     date: Date,
     pounds: Number,
     fulfilled: Boolean,
 })
 
-var ItemSchema = new Schema({
+const ItemSchema = new Schema({
     name: { type: String, unique: true },
     itemid: { type: String, unique: true },
     batches: [BatchSchema],
 })
 
-var Item = mongoose.model('Item', ItemSchema);
+const Item = mongoose.model('Item', ItemSchema);
 module.exports = Item;
