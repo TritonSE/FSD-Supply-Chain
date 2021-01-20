c891ac96774e081f1eb1ddad7f6b58c183186bb9
Ly <64985802+lynguyxn@users.noreply.github.com>
2021-01-20 14:44:31 -0800

    Changed to const variables

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
-var mongoose = require('mongoose');
-var Item = require('./models/item');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors");
+const mongoose = require('mongoose');
+const Item = require('./models/item');
 
 var indexRouter = require('./routes/index');
 
