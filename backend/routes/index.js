68356536d0ff9bc4d8485a51869a32b5a9328851
Philip Zhang <jmslca123@gmail.com>
2021-01-20 18:53:15 -0800

    fix typo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Sup.ply Chain");
+  res.send("Supply Chain");
 });
 
 module.exports = router;
