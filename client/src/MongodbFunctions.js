205f19455c0b0858e9db359b1368f511dd25c252
Anson <all197@ucsd.edu>
2021-01-25 15:54:44 -0800

    updated post request

--- client/src/MongodbFunctions.js
+++ client/src/MongodbFunctions.js
@@ -1,5 +1,9 @@
 export const postNewItem = (itemName, itemId, weight, outByDate) => {
-    console.log("Need to implement post to MongoDB", itemName, itemId, weight, outByDate);
+    console.log("Need to implement post to MongoDB");
+    console.log("itemName: ", itemName);
+    console.log("itemId: ", itemId);
+    console.log("weight: ", weight);
+    console.log("outByDate: ", outByDate);
     // fetch('url', {
     //     method: 'POST',
     //     body: JSON.stringify({
