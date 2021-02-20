2048f9087892c5377ec5a3fef40558dfbceb9ad2
Anson <36427506+AnsonLee99@users.noreply.github.com>
2021-02-20 15:53:47 -0800

    autocomplete for items in add-item-form (#15)

--- client/src/home/sidebar/Sidebar.js
+++ client/src/home/sidebar/Sidebar.js
@@ -1,61 +1,26 @@
 import React, {useState, useEffect} from 'react';
 
 import {useCookies} from 'react-cookie';
-import {getAllItems} from '../../MongodbFunctions';
 import {Form} from 'react-bootstrap';
 
 import {SidebarItemAlpha, SidebarItemExpiry} from './SidebarItem';
 
 import './Sidebar.scss';
 
-function Sidebar() {
+function Sidebar(props) {
   const [itemsAlpha, setItemsAlpha] = useState([]);
   const [itemsExpiry, setItemsExpiry] = useState([]);
   const [cookies] = useCookies(['token']);
   const [sortBy, setSortBy] = useState('Alphabetical');
   const [expandAll, setExpandAll] = useState(false);
 
-  // Sort by Alphabetical order
-  const sortAlpha = (a, b) => {
-    return a.name > b.name;
-  }
-
-  // Sort by Expiry order
-  const sortExpiry = (a, b) => {
-    a = new Date(a.outDate);
-    b = new Date(b.outDate);
-    let diff = a.getTime() - b.getTime();
-    if (diff !== 0) {
-      return diff;
-    }
-    
-    // If time is the same, sort by alphabetical order
-    return a.name > b.name;
-  }
-
-  // Retrieve all items from MongoDB 
   useEffect(() => {
-    getAllItems(cookies.token).then(items_raw => {
-      var batches_raw = [];
+    setItemsAlpha(props.itemsAlpha);
+  }, [props.itemsAlpha])
 
-      if (items_raw != null) {
-        items_raw.forEach(item => {
-          // Format item names
-          item.name = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-
-          // Format outDates and prepare batches_raw for sorting by expiry
-          item.batches.forEach(batch => {
-            batch.outDate = new Date(batch.outDate).toISOString().replace(/^\d+-0?(\d+)-0?(\d+)T.*$/, "$1/$2");
-            batch.itemName = item.name;
-            batches_raw.push(batch);
-          })
-        });
-        
-        setItemsAlpha(items_raw.sort(sortAlpha));
-        setItemsExpiry(batches_raw.sort(sortExpiry))
-      }
-    })
-  }, []);
+  useEffect(() => {
+    setItemsExpiry(props.itemsExpiry);
+  }, [props.itemsExpiry])
 
   return (
     <div className='sidebar_container'>
