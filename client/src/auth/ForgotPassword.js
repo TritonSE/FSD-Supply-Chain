6fbd035e8f467e9079fd9997b4cbca08a29e96e7
Philip Zhang <jmslca123@gmail.com>
2021-02-26 17:39:36 -0800

    implement forgot password flow
    Co-authored-by: Benson Vuong <bensonvuong275@gmail.com>

---
+++ client/src/auth/ForgotPassword.js
@@ -0,0 +1,68 @@
+import React, { useState, useMemo } from "react";
+import { navigate, Link } from "@reach/router";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { forgotPassword } from "../MongodbFunctions";
+import userIcon from "../assets/icons/user.svg";
+
+import "./auth.scss";
+
+const ResetPassword = (props) => {
+  // Input fields
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    forgotPassword(email).then(async (response) => {
+      const body = await response.json();
+      setResponse(response.status);
+      setMessage(body.message);
+    });
+  };
+
+  return (
+    <div className="auth_form_container">
+      <Form className="auth_form" onSubmit={submitForm}>
+        <h1 className="auth_header">Feeding San Diego</h1>
+        <p className="white_text">
+          To reset your password, enter in your registered email and a reset
+          link will be emailed to you.
+        </p>
+        <Form.Group className="input_field_container">
+          <img class="icons" src={userIcon} alt="user icon"></img>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            onChange={(event) => {
+              setEmail(event.target.value.trim());
+            }}
+            required
+          />
+        </Form.Group>
+
+        {message.length ? <p className="white_text">{message}</p> : ""}
+        {response !== 200 ? (
+          <Button
+            type="submit"
+            onClick={submitForm}
+            className={email.length > 0 ? "active" : ""}
+          >
+            Send Link
+          </Button>
+        ) : (
+          ""
+        )}
+
+        <Link to="/login" className="white_text redirect sub_button">
+          {response !== 200 ? "Cancel" : "Back to Log In"}
+        </Link>
+      </Form>
+    </div>
+  );
+};
+
+export default ResetPassword;
--- client/src/auth/ResetPassword.js
+++ client/src/auth/ResetPassword.js
@@ -3,52 +3,50 @@ import { navigate } from "@reach/router";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { login } from "../MongodbFunctions";
+import { resetPassword } from "../MongodbFunctions";
 import { useCookies } from "react-cookie";
 import userIcon from "../assets/icons/user.svg";
 import passwordIcon from "../assets/icons/password.svg";
 
 import "./auth.scss";
 
-const Login = (props) => {
+const ResetPassword = (props) => {
   // Input fields
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
-  const [cookies, setCookie] = useCookies(["token"]);
 
   const submitForm = (e) => {
     e.preventDefault();
-    login(email, password).then(async (response) => {
+    if (password !== confirmPassword) {
+      setError("The passwords do not match");
+      return;
+    }
+    resetPassword(props.token, password).then(async (response) => {
       const body = await response.json();
-      if (email.length === 0 || password.length === 0) {
-        setError("Please fill in all fields");
-        return;
-      }
       if (!response.ok) {
         setError(body.message);
       } else {
-        setCookie("token", body.token);
-        navigate("/");
+        navigate("/login");
       }
     });
   };
 
   const allFieldsFilled = useMemo(() => {
-    return email.length !== 0 && password.length !== 0;
-  }, [email, password]);
+    return confirmPassword.length !== 0 && password.length !== 0;
+  }, [confirmPassword, password]);
 
   return (
     <div className="auth_form_container">
       <Form className="auth_form" onSubmit={submitForm}>
         <h1 className="auth_header">Feeding San Diego</h1>
         <Form.Group className="input_field_container">
-          <img class="icons" src={userIcon} alt="user icon"></img>
+          <img class="icons" src={passwordIcon} alt="password icon"></img>
           <Form.Control
-            type="text"
-            placeholder="Email"
+            type="password"
+            placeholder="New Password"
             onChange={(event) => {
-              setEmail(event.target.value.trim());
+              setPassword(event.target.value.trim());
             }}
             required
           />
@@ -58,18 +56,13 @@ const Login = (props) => {
           <img class="icons" src={passwordIcon} alt="password icon"></img>
           <Form.Control
             type="password"
-            placeholder="Password"
+            placeholder="Confirm Password"
             onChange={(event) => {
-              setPassword(event.target.value.trim());
+              setConfirmPassword(event.target.value.trim());
             }}
             required
           />
         </Form.Group>
-        <p className="white_text redirect sub_link">
-          <a href="/forgot" className="white_text">
-            Forgot Password
-          </a>
-        </p>
 
         {error.length ? <p className="warning">{error}</p> : ""}
         <Button
@@ -77,17 +70,11 @@ const Login = (props) => {
           onClick={submitForm}
           className={allFieldsFilled ? "active" : ""}
         >
-          Log In
+          Reset Password
         </Button>
-
-        <p className="white_text redirect">
-          <a href="/signup" className="white_text">
-            Create an account
-          </a>
-        </p>
       </Form>
     </div>
   );
 };
 
-export default Login;
+export default ResetPassword;
